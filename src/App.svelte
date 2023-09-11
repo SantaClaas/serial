d699cd7f4a33e5072f50156c6e6f1bb6b8e507d6
Claas <claas.moehlmann@outlook.com>
2023-09-11 17:14:55 +0200

    Add write and close

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  console.log("start");
+  import { readable, writable } from "svelte/store";
 
   function isSerialPort(object: EventTarget | null): object is SerialPort {
     return object !== null && "onconnect" in object;
@@ -33,22 +33,55 @@
       .then((event) => console.log("port requested"));
   }
 
-  function send(port: SerialPort) {
+  function quickUpdateHack() {
+    // TODO remove this hack to force rerender
+    availablePorts = availablePorts;
+  }
+
+  function open(port: SerialPort) {
     return async () => {
-      console.log("Send on port", port);
+      console.log("Opening port", port);
       await port.open({ baudRate: 9600 });
+      quickUpdateHack();
+      console.log("Port opened", port.getInfo());
+    };
+  }
+
+  function close(port: SerialPort) {
+    return async () => {
+      await port.close();
+      quickUpdateHack();
+    };
+  }
+
+  const data = new Uint8Array([104, 101, 108, 108, 111]); // hello
+  function write(port: SerialPort) {
+    return async () => {
+      console.log("Writing port");
+      const writer = port.writable.getWriter();
+      await writer.write(data);
+      writer.releaseLock();
+      console.log("Wrote to port");
     };
   }
 </script>
 
 <main>
-  <h2>Devices:</h2>
+  <h2>Serial Ports:</h2>
   <ul>
     {#each availablePorts as [port, info]}
       <li>
         <p>USB Product Id: {info.usbProductId}</p>
         <p>Vendor Id: {info.usbVendorId}</p>
-        <button on:click={send(port)}>Send</button>
+
+        {#if port.writable || port.readable}
+          {#if port.writable}
+            <button on:click={write(port)}>Write</button>
+          {/if}
+          <button on:click={close(port)}>Close</button>
+        {:else}
+          <button on:click={open(port)}>Open</button>
+        {/if}
       </li>
     {/each}
   </ul>
