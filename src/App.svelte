e737b9b0ca67fe8df0c00b6d2925211a95694dab
Claas <claas.moehlmann@outlook.com>
2023-09-12 15:56:43 +0200

    Fix running subscription multiple times

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,28 @@
 <script lang="ts">
-  import { readable, writable } from "svelte/store";
+  import { onMount } from "svelte";
 
   function isSerialPort(object: EventTarget | null): object is SerialPort {
     return object !== null && "onconnect" in object;
   }
 
   let availablePorts: Map<SerialPort, Partial<SerialPortInfo>> = new Map();
-  navigator.serial.addEventListener("connect", (event) => {
-    if (!isSerialPort(event.target)) return;
 
-    console.log("connected", event.target, typeof event.target);
-    availablePorts.set(event.target, event.target.getInfo());
+  onMount(() => {
+    navigator.serial.addEventListener("connect", (event) => {
+      if (!isSerialPort(event.target)) return;
+
+      console.log("connected", event.target, typeof event.target);
+      availablePorts.set(event.target, event.target.getInfo());
+    });
+
+    navigator.serial.addEventListener("disconnect", (event) => {
+      if (!isSerialPort(event.target)) return;
+
+      console.log("disconnected", event.target);
+      availablePorts.delete(event.target);
+    });
+    console.log("Event listener added");
   });
-
-  navigator.serial.addEventListener("disconnect", (event) => {
-    if (!isSerialPort(event.target)) return;
-
-    console.log("disconnected", event.target);
-    availablePorts.delete(event.target);
-  });
-
-  console.log("Event listener added");
 
   navigator.serial.getPorts().then((ports) => {
     availablePorts = new Map(ports.map((port) => [port, port.getInfo()]));
