05ddbeb9d9e6f565ed78dbc96edaeaf028abab19
Claas <claas.moehlmann@outlook.com>
2023-09-15 14:38:12 +0200

    Enable closing a serial port

--- src/lib/SerialPortSettings.svelte
+++ src/lib/SerialPortSettings.svelte
@@ -8,39 +8,49 @@
   let selectedRate: BaudRate = 9600;
   let isOpen = !!port.writable && !!port.readable;
 
-  async function openPort(event: Event) {
-    console.log("Opening port");
+  async function openPort() {
+    if (isOpen) return;
+
     await port.open({ baudRate: selectedRate });
 
     isOpen = !!port.writable && !!port.readable;
-    console.log("Port opened");
+  }
+
+  async function closePort() {
+    if (!isOpen) return;
+
+    await port.close();
+
+    isOpen = !!port.writable && !!port.readable;
   }
 </script>
 
-<form on:submit|preventDefault={openPort}>
-  <fieldset class="port-settings">
-    <legend>Serial Port Settings</legend>
-    <label for="vendor-id">USB Vendor Id</label>
-    <input
-      id="vendor-id"
-      type="number"
-      value={info.usbVendorId}
-      readonly
-      disabled
-    />
-    <label for="product-id">USB Product Id</label>
-    <input
-      id="product-id"
-      type="number"
-      value={info.usbProductId}
-      readonly
-      disabled
-    />
+<fieldset class="port-settings">
+  <legend>Serial Port Settings</legend>
+  <label for="vendor-id">USB Vendor Id</label>
+  <input
+    id="vendor-id"
+    type="number"
+    value={info.usbVendorId}
+    readonly
+    disabled
+  />
+  <label for="product-id">USB Product Id</label>
+  <input
+    id="product-id"
+    type="number"
+    value={info.usbProductId}
+    readonly
+    disabled
+  />
 
-    <BaudRateSelect bind:value={selectedRate} disabled={isOpen} />
-    <button type="submit" disabled={isOpen}>Open</button>
-  </fieldset>
-</form>
+  <BaudRateSelect bind:value={selectedRate} disabled={isOpen} />
+
+  <div>
+    <button disabled={!isOpen} on:click={closePort}>Close</button>
+    <button disabled={isOpen} on:click={openPort}>Open</button>
+  </div>
+</fieldset>
 
 {#if isOpen}
   <ModbusSettings baudrate={selectedRate} {port} />
@@ -52,7 +62,8 @@
     gap: 1rem;
     grid-template-columns: repeat(2, auto);
 
-    & button[type="submit"] {
+    & div {
+      justify-self: end;
       grid-column: 2 / 3;
     }
   }
