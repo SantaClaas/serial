f98e6b6ffb5c9f1cf2d708dcdffec7efad7baa7a
Claas <claas.moehlmann@outlook.com>
2023-09-26 18:23:35 +0200

    Handle rare cases when registers are not defined

--- src/lib/DeviceView.svelte
+++ src/lib/DeviceView.svelte
@@ -5,20 +5,32 @@
   export let device: Device;
 
   export let disabled: boolean;
+
+  const countInputRegisters = Object.keys(device.inputRegisters).length,
+    countHoldingRegisters = Object.keys(device.holdingRegisters).length;
 </script>
 
-<fieldset class="input-registers" {disabled}>
-  <legend>Input registers</legend>
-  {#each Object.entries(device.inputRegisters) as [label, register]}
-    <Register bind:disabled {register} {label} />
-  {/each}
-</fieldset>
-<fieldset class="holding-registers" {disabled}>
-  <legend>Holding registers</legend>
-  {#each Object.entries(device.holdingRegisters) as [label, register]}
-    <Register bind:disabled {register} {label} />
-  {/each}
-</fieldset>
+{#if countInputRegisters}
+  <fieldset class="input-registers" {disabled}>
+    <legend>Input registers</legend>
+    {#each Object.entries(device.inputRegisters) as [label, register]}
+      <Register bind:disabled {register} {label} />
+    {/each}
+  </fieldset>
+{/if}
+
+{#if countHoldingRegisters}
+  <fieldset class="holding-registers" {disabled}>
+    <legend>Holding registers</legend>
+    {#each Object.entries(device.holdingRegisters) as [label, register]}
+      <Register bind:disabled {register} {label} />
+    {/each}
+  </fieldset>
+{/if}
+
+{#if !countInputRegisters && !countInputRegisters}
+  <p>No registers defined for this device</p>
+{/if}
 
 <style>
   fieldset {
